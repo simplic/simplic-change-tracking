2df9c3acc8016cde1c4e44ea0fd5015943c158a2
tanyel.tuncer@spiegelburg.biz <tanyel.tuncer@spiegelburg.biz>
2020-05-18 16:38:48 +0200

    Added a ui

---
+++ simplic-change-tracking/Simplic.Change.Tracking.UI/RequestChangeApplicationHelper.cs
@@ -0,0 +1,13 @@
+﻿using Simplic.Framework.DBUI;
+using System;
+
+
+namespace Simplic.Change.Tracking.UI
+{
+    public class RequestChangeApplicationHelper : GridWindowApplicationHelper<int, RequestChange, RequestChangeViewModel>
+    {
+        public override string PrimaryKeyColumn => "Ident";
+
+        public override Type WindowInterface => typeof(IRequestChangeWindow);
+    }
+}
---
+++ simplic-change-tracking/Simplic.Change.Tracking.UI/RequestChangeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Simplic.Framework.UI;
+using Simplic.Studio.UI;
+
+namespace Simplic.Change.Tracking.UI
+{
+    public class RequestChangeViewModel : ExtendableViewModel, IWindowViewModel<RequestChange>
+    {
+    }
+}
